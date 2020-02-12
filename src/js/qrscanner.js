f14fdbed8a704703f0f11c225c715919dd2ebf20
QVDev <feedbackandroid@gmail.com>
2020-02-12 16:01:01 +0100

    Clean up camera correctly and directly login without event dispatcher

--- src/js/qrscanner.js
+++ src/js/qrscanner.js
@@ -1,37 +1,26 @@
-window.addEventListener('load', function () {
-    let selectedDeviceId;
-    let recording = false;
-    const codeReader = new ZXing.BrowserMultiFormatReader()
-    codeReader.getVideoInputDevices()
-        .then((videoInputDevices) => {
-            document.getElementById('startButton').addEventListener('click', () => {
-                if (recording) {
-                  codeReader.reset();
-                  recording = false;
-                  return;
-                }
-                recording = true;
-                codeReader.decodeFromVideoDevice(null, 'video', (result, err) => {
-                    if (result) {
-                        var privkey = document.getElementById('paste-privkey')
-                        privkey.value = result.text
+var codeReader;
 
-                        var event = new Event('input', {
-                            bubbles: true,
-                            cancelable: true,
-                        });
+function startQRScanner() {
+    codeReader = new ZXing.BrowserMultiFormatReader();
+    codeReader.decodeFromInputVideoDevice(undefined, 'video')
+        .then(result => {
+            var qr = JSON.parse(result.text);
+            if (qr.priv != undefined) {
+                cleanupScanner()
+                login(qr);
+            }
+        }).catch(err => {
+            if (err != undefined) {
+                console.error(err)
+            }
+            if (codeReader != undefined) {
+                cleanupScanner()
+            }
+        });
+}
 
-                        privkey.dispatchEvent(event);
-                        codeReader.reset();
-                        recording = false;
-                    }
-                    if (err && !(err instanceof ZXing.NotFoundException)) {
-                        console.error(err)
-                    }
-                })
-            })
-        })
-        .catch((err) => {
-            console.error(err)
-        })
-})
+function cleanupScanner() {
+    if (codeReader != undefined) {
+        codeReader.reset();
+    }
+}
