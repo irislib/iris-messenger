ec5ce4f801090b199db2b1e1b9d2ab0fdf230006
thrownness <57295746+thrownness@users.noreply.github.com>
2020-04-06 14:38:54 -0400

    add pinning functionality

--- src/js/app.js
+++ src/js/app.js
@@ -685,6 +685,13 @@ function showProfile(pub) {
     console.log('add friend');
   });
   $('#profile .delete-chat').off().on('click', () => deleteChat(pub));
+  $('#profile .pin-chat').off().click(() => {
+    if (chats[pub].pinned) {
+      chats[pub].put('pinned', false);
+    } else {
+      chats[pub].put('pinned', true);
+    }
+  });
   $('#profile .send-message').off().on('click', () => showChat(pub));
   $('#profile .copy-user-link').off().on('click', event => {
     copyToClipboard(link);
@@ -838,7 +845,9 @@ function getIdenticon(pub, width) {
 
 var sortChatsByLatest = _.throttle(() => {
   var sorted = $(".chat-item:not(.new)").sort((a, b) => {
-    return ($(b).data('latestTime') || -Infinity) - ($(a).data('latestTime') || -Infinity);
+    var aValue = $(a).hasClass('pinned') ? Infinity : ($(a).data('latestTime') || -Infinity)
+    var bValue = $(b).hasClass('pinned') ? Infinity : ($(b).data('latestTime') || -Infinity)
+    return bValue - aValue;
   });
   $(".chat-list").append(sorted);
 }, 100);
@@ -863,6 +872,7 @@ function sortMessagesByTime() {
 }
 
 var seenIndicatorHtml = '<span class="seen-indicator"><svg version="1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59 42"><polygon fill="currentColor" points="40.6,12.1 17,35.7 7.4,26.1 4.6,29 17,41.3 43.4,14.9"></polygon><polygon class="iris-delivered-checkmark" fill="currentColor" points="55.6,12.1 32,35.7 29.4,33.1 26.6,36 32,41.3 58.4,14.9"></polygon></svg></span>';
+var pinnedIndicatorHTML = '<span class="pinned-indicator"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z"/></svg>'
 
 function addMessage(msg) {
   var escaped = $('<div>').text(msg.text).html();
@@ -931,6 +941,22 @@ function addChat(pub, chatLink) {
       }
       el.find('.latest-time').text(latestTimeText);
       el.data('latestTime', msg.time);
+      chats[pub].on('pinned',(p) => {
+        //console.log('detected pin: ',p);
+        chats[pub].pinned = p;
+        if (chats[pub].pinned) {
+          if (el.hasClass( 'pinned' )) {
+          } else {
+            el.addClass( 'pinned' );
+            el.prepend($(pinnedIndicatorHTML))
+            sortChatsByLatest();
+          }
+        } else {
+          el.removeClass( 'pinned' );
+          el.find('.pinned-indicator').remove();
+          sortChatsByLatest();
+        }
+      },"me");
       sortChatsByLatest();
     }
     if (activeChat === pub) {
