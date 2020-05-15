bc92461f327ba58f21ae048166eb562b15bfa283
Martti Malmi <sirius@iki.fi>
2020-05-15 15:05:44 +0300

    no sw caching on localhost

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,16 +1,18 @@
 var CACHE_NAME = 'iris-messenger-cache-v1';
 
-// stale-while-revalidate caching strategy
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        var fetchPromise = fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
+// stale-while-revalidate
+if (self.location.host.indexOf('localhost') !== 0) {
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          var fetchPromise = fetch(event.request).then(function(networkResponse) {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
         })
-        return response || fetchPromise;
       })
-    })
-  );
-});
+    );
+  });
+}
