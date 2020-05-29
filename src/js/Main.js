619482cf99ae08753707717b8542ee51abc647ac
Martti Malmi <sirius@iki.fi>
2020-05-29 16:25:08 +0300

    <br> -> <br/> :D

--- src/js/Main.js
+++ src/js/Main.js
@@ -35,7 +35,7 @@ const app = html`
             <h1>${t('iris_messenger')}</h1>
             <input autofocus autocomplete="off" autocorrect="off" autocapitalize="sentences" spellcheck="off" id="login-form-name" type="text" name="name" placeholder="${t('whats_your_name')}"/>
             <p><button id="sign-up" type="submit">${t('new_user_go')}</button></p>
-            <br>
+            <br/>
             <p><a href="#" id="show-existing-account-login">${t('already_have_an_account')}</a></p>
             <p>
               <svg width="14" height="14" style="margin-bottom: -1px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 469.333 469.333" style="enable-background:new 0 0 469.333 469.333;" xml:space="preserve"><path fill="currentColor" d="M253.227,300.267L253.227,300.267L199.04,246.72l0.64-0.64c37.12-41.387,63.573-88.96,79.147-139.307h62.507V64H192 V21.333h-42.667V64H0v42.453h238.293c-14.4,41.173-36.907,80.213-67.627,114.347c-19.84-22.08-36.267-46.08-49.28-71.467H78.72 c15.573,34.773,36.907,67.627,63.573,97.28l-108.48,107.2L64,384l106.667-106.667l66.347,66.347L253.227,300.267z"/><path fill="currentColor" d="M373.333,192h-42.667l-96,256h42.667l24-64h101.333l24,64h42.667L373.333,192z M317.333,341.333L352,248.853 l34.667,92.48H317.333z"/></svg>
@@ -317,11 +317,11 @@ const app = html`
           <div class="notification-settings">
             <h4>${t('notifications')}</h4>
             <input type="radio" id="notifyAll" name="notificationPreference" value="all"/>
-            <label for="notifyAll">${t('all_messages')}</label><br>
+            <label for="notifyAll">${t('all_messages')}</label><br/>
             <input type="radio" id="notifyMentionsOnly" name="notificationPreference" value="mentions"/>
-            <label for="notifyMentionsOnly">${t('mentions_only')}</label><br>
+            <label for="notifyMentionsOnly">${t('mentions_only')}</label><br/>
             <input type="radio" id="notifyNothing" name="notificationPreference" value="nothing"/>
-            <label for="notifyNothing">${t('nothing')}</label><br>
+            <label for="notifyNothing">${t('nothing')}</label><br/>
           </div>
           <hr/>
           <p>
