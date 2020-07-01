0283bb0537216b9d3bcd0c650f2270c8a9d1f6b2
Martti Malmi <sirius@iki.fi>
2020-07-01 18:00:35 +0300

    handle push notif click

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -25,3 +25,28 @@ self.addEventListener('push', ev => {
     icon: './img/icon128.png'
   });
 });
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  // Android doesn't close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(
+    clients.matchAll({
+      type: "window"
+    })
+    .then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url == '/' && 'focus' in client)
+          return client.focus();
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
