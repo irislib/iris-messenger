d06811b2bdab4536cc60a94076b50087fbefc6da
Martti Malmi <sirius@iki.fi>
2020-08-07 16:46:52 +0300

    profile photo fix

--- src/js/Profile.js
+++ src/js/Profile.js
@@ -296,6 +296,7 @@ function renderProfilePhotoSettings() {
     if (!$('#current-profile-photo').attr('src')) {
       $('#add-profile-photo').show();
     }
+    $('#current-profile-photo').show();
     Helpers.setImgSrc($('#profile-photo-preview'), '');
     $('#profile-photo-preview').toggleClass('hidden', true);
     $('#cancel-profile-photo').toggleClass('hidden', true);
