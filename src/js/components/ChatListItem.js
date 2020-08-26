994286808625bc9e6b27e482f4b360133ff40c0d
Martti Malmi <sirius@iki.fi>
2020-08-26 11:25:10 +0300

    emoji-picker, note to self

--- src/js/components/ChatListItem.js
+++ src/js/components/ChatListItem.js
@@ -2,6 +2,7 @@ import { html, Component } from '../lib/htm.preact.js';
 import {showChat} from '../Chat.js';
 import { translate as t } from '../Translation.js';
 import {localState} from '../Main.js';
+import Session from '../Session.js';
 import Identicon from './Identicon.js';
 
 const seenIndicator = html`<span class="seen-indicator"><svg version="1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 59 42"><polygon fill="currentColor" points="40.6,12.1 17,35.7 7.4,26.1 4.6,29 17,41.3 43.4,14.9"></polygon><polygon class="iris-delivered-checkmark" fill="currentColor" points="55.6,12.1 32,35.7 29.4,33.1 26.6,36 32,41.3 58.4,14.9"></polygon></svg></span>`;
@@ -55,6 +56,11 @@ class ChatListItem extends Component {
     const seen = chat.theirMsgsLastSeenTime >= chat.latestTime ? "seen" : "";
     const delivered = chat.theirLastActiveTime >= chat.latestTime ? "delivered" : "";
 
+    let name = chat.name;
+    if (chat.id === Session.getKey().pub) {
+      name = html`📝 <b>${t('note_to_self')}</b>`;
+    }
+
     const photo = this.props.photo;
     let iconEl;
     if (photo) {
@@ -67,7 +73,7 @@ class ChatListItem extends Component {
       ${iconEl}
       <div class="text">
         <div>
-          <span class="name">${this.state.chat.name}</span>
+          <span class="name">${name}</span>
           <small class="latest-time">${this.state.latestTimeText}</small>
         </div>
         <small class="typing-indicator">${t('typing')}</small>
