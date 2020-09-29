d7f98592b13f698397e18359691cdee7546acdf5
Martti Malmi <sirius@iki.fi>
2020-09-29 14:56:58 +0300

    v1.5.0

--- src/js/components/Settings.js
+++ src/js/components/Settings.js
@@ -81,7 +81,7 @@ class Settings extends Component {
             <li><b>Available</b>: It works offline-first and is not dependent on any single centrally managed server. Users can even connect directly to each other.</li>
           </ul>
           <p>Released under MIT license. Code: <a href="https://github.com/irislib/iris-messenger">Github</a>.</p>
-          <p><small>Version 1.4.0</small></p>
+          <p><small>Version 1.5.0</small></p>
 
           <div id="desktop-application-about">
             <h4>Get the desktop application</h4>
