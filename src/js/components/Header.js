1fe35e92ba19d9acea1735b66d7794dd51357759
Martti Malmi <sirius@iki.fi>
2020-09-29 14:00:27 +0300

    add logotype

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -135,7 +135,10 @@ class Header extends Component {
           <span class="unseen unseen-total"></span>
         </div>
         ` : ''}
-        <a href="/" class=${activeRoute && activeRoute.indexOf('/chat/') === 0 ? 'hidden-xs' :'' }><img src="img/icon128.png" width="40" height="40"/></a>
+        <a href="/" class="${activeRoute && activeRoute.indexOf('/chat/') === 0 ? 'hidden-xs' :'' } logo">
+          <img src="img/icon128.png" width=40 height=40/>
+          <img src="img/iris_logotype.png" height=23 width=41 class=${activeRoute && activeRoute.indexOf('/profile/') === 0 ? 'hidden-xs':''}/>
+        </a>
         <div class="text" style=${this.chatId ? 'cursor:pointer' : ''} onClick=${() => this.onTitleClicked()}>
           <div class="name">
             ${this.state.title || ''}
