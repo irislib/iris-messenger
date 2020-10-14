1e74ec72686dd448bfc1b7469955fb4e59a59130
Martti Malmi <sirius@iki.fi>
2020-10-14 14:36:12 +0300

    .

--- src/js/components/Identicon.js
+++ src/js/components/Identicon.js
@@ -1,5 +1,6 @@
 import { Component } from '../lib/preact.js';import { html } from '../Helpers.js';
 import Helpers from '../Helpers.js';
+import {publicState} from '../Main.js';
 
 class Identicon extends Component {
   constructor() {
