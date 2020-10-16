440044f2d4841c18fca56cd1d9783671006d66ae
Martti Malmi <sirius@iki.fi>
2020-10-16 13:21:57 +0300

    redirect to feed after posting a message on mobile

--- src/js/components/MessageView.js
+++ src/js/components/MessageView.js
@@ -2,13 +2,14 @@ import { Component } from '../lib/preact.js';
 import { html } from '../Helpers.js';
 import PublicMessage from './PublicMessage.js';
 import MessageForm from './MessageForm.js';
+import { route } from '../lib/preact-router.es.js';
 
 class MessageView extends Component {
   render() {
     let content;
     if (this.props.hash === 'new') {
       content = html`
-        <${MessageForm} activeChat="public" autofocus=${true}/>
+        <${MessageForm} activeChat="public" autofocus=${true} onSubmit=${() => route('/')}/>
       `;
     } else {
       content = html`
