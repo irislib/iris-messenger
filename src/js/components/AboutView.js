c1134d343ac5da7e87cbcedaa38ea1e7df6b7290
Martti Malmi <sirius@iki.fi>
2021-01-22 15:33:34 +0200

    version

--- src/js/components/AboutView.js
+++ src/js/components/AboutView.js
@@ -13,7 +13,7 @@ export default () => {
           <li><b>Available</b>: It works offline-first and is not dependent on any single centrally managed server. Users can even connect directly to each other.</li>
         </ul>
         <p>Released under MIT license. Code: <a href="https://github.com/irislib/iris-messenger">Github</a>.</p>
-        <p><small>Version 1.5.3</small></p>
+        <p><small>Version 1.5.4</small></p>
 
         <div id="desktop-application-about">
           <h4>Get the desktop application</h4>
