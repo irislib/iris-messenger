c07286432029089b1f7d32866bf57217a3da2f2d
Martti Malmi <sirius@iki.fi>
2021-02-17 10:22:19 +0200

    restore iris header logo in mobile size

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -116,6 +116,12 @@ class Header extends Component {
       </div>
       ` : ''}
       <div class="header-content">
+        ${iris.util.isElectron ? '' : html`
+          <a href="/" onClick=${e => this.onLogoClick(e)} tabindex="0" class="${activeRoute && activeRoute.indexOf('/chat/') === 0 ? 'hidden-xs' :'visible-xs-flex' } logo">
+            <img src="img/icon128.png" width=40 height=40/>
+            <img src="img/iris_logotype.png" height=23 width=41 />
+          </a>
+        `}
         <div class="text" style=${this.chatId ? 'cursor:pointer' : ''} onClick=${() => this.onTitleClicked()}>
           ${this.state.title && activeRoute && activeRoute.indexOf('/chat/') === 0 ? html`
             <div class="name">
