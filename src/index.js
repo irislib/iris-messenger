7008ed681c7b5dc2dfb7bec4c33f273574543855
Martti Malmi <sirius@iki.fi>
2021-07-09 02:40:40 +0300

    rm size-plugin.json, manifest.json

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ export default class App extends Component {
             <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png"/>
             <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png"/>
             <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png"/>
-            <link rel="manifest" href="static/site.webmanifest"/>
+            <link rel="manifest" href="manifest.json"/>
             <link rel="mask-icon" href="assets/img/safari-pinned-tab.svg" color="#74d5f1"/>
             <link rel="shortcut icon" href="/favicon.ico"/>
             <meta name="msapplication-TileColor" content="#74d5f1"/>
