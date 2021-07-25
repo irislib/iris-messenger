db68d4fa2f63cd28e3c94ee2a9b7805aff44abba
UmairMukhtar <umairmukhtar27@gmail.com>
2021-07-25 18:07:41 +0500

    'infinitescroll

--- src/js/components/MessageFeed.js
+++ src/js/components/MessageFeed.js
@@ -1,30 +1,63 @@
-import { Component } from "preact";
-import Helpers, { html } from "../Helpers.js";
-import PublicMessage from "./PublicMessage.js";
-import {
-  List,
-  WindowScroller,
-  CellMeasurer,
-  CellMeasurerCache,
-} from "react-virtualized";
-import State from "../State.js";
-import "react-virtualized/styles.css";
-import $ from "jquery";
+import { Component } from 'preact';
+import Helpers, { html } from '../Helpers.js';
+import PublicMessage from './PublicMessage.js';
+import {  List, WindowScroller,CellMeasurer,CellMeasurerCache,} from 'react-virtualized';
+import State from '../State.js';
+import 'react-virtualized/styles.css';
+import $ from 'jquery';
 
 class MessageFeed extends Component {
   constructor() {
     super();
-    this.state = { sortedMessages: [] };
+    this.state = {sortedMessages:[]};
     this.mappedMessages = new Map();
     this.eventListeners = {};
     this._cache = new CellMeasurerCache({
       fixedWidth: true,
-      minHeight: 50,
+      minHeight: 100,
     });
     this.rowRenderer = this.rowRenderer.bind(this);
   }
 
-  rowRenderer = ({ index, key, parent, style }) => {
+  componentDidMount() {
+    this.props.node.map().on((v, k, x, e) => {
+      if (!this.eventListeners['node']) {
+        this.eventListeners['node'] = e;
+      }
+      if (v) {
+        this.mappedMessages.set(k, this.props.keyIsMsgHash ? k : v);
+      } else {
+        this.mappedMessages.delete(k);
+      }
+      this.setState({
+        sortedMessages: Array.from(this.mappedMessages.keys()).sort().reverse().map(k => this.mappedMessages.get(k))
+      })
+    });
+    let first = true;
+    State.local.get('scrollUp').on(() => {
+      !first && Helpers.animateScrollTop('.main-view');
+      first = false;
+    });
+  }
+
+  unsubscribe() {
+    Object.values(this.eventListeners).forEach(e => e.off());
+    this.eventListeners = {};
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.node._.id !== prevProps.node._.id) {
+      this.unsubscribe();
+      this.setState({sortedMessages: [], mappedMessages: new Map()});
+    }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  rowRenderer = ({ index, key, parent, style,isScrolling }) => {
+    // console.log("THiss is scrolling",isScrolling)
     const hash = this.state.sortedMessages[index];
     const msg =
       typeof hash === "string"
@@ -45,6 +78,7 @@ class MessageFeed extends Component {
       >
         {({ measure, registerChild }) => (
           <div ref={registerChild} style={style}>
+            {measure()}
             {typeof hash === "string"
               ? html`<${PublicMessage}
                   filter=${this.props.filter}
@@ -56,82 +90,30 @@ class MessageFeed extends Component {
                   showName=${true}
                 />`
               : ""}
-            <img
-              src
-              style={{ display: "none" }}
-              onError={() => {
-                console.log("This is topest measure");
-                measure();
-              }}
-            ></img>
           </div>
         )}
       </CellMeasurer>
     );
   };
 
-  componentDidMount() {
-    this.props.node.map().on((v, k, x, e) => {
-      if (!this.eventListeners["node"]) {
-        this.eventListeners["node"] = e;
-      }
-      if (v) {
-        this.mappedMessages.set(k, this.props.keyIsMsgHash ? k : v);
-      } else {
-        this.mappedMessages.delete(k);
-      }
-      this.setState({
-        sortedMessages: Array.from(this.mappedMessages.keys())
-          .sort()
-          .reverse()
-          .map((k) => this.mappedMessages.get(k)),
-      });
-    });
-    let first = true;
-    State.local.get("scrollUp").on(() => {
-      !first && Helpers.animateScrollTop(".main-view");
-      first = false;
-    });
-  }
-
-  unsubscribe() {
-    Object.values(this.eventListeners).forEach((e) => e.off());
-    this.eventListeners = {};
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.node._.id !== prevProps.node._.id) {
-      this.unsubscribe();
-      this.setState({ sortedMessages: [], mappedMessages: new Map() });
-    }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
   render() {
     return html`
       <div class="feed-container">
-          <${WindowScroller} scrollElement=${$(".main-view")[0]}>
-            ${({ height, width, isScrolling, onChildScroll, scrollTop }) => {
-              console.log("THis is cache", this._cache._rowHeightCache);
-
-              return html`
-                <${List}
-                  autoHeight
-                  autoWidth
-                  width=${width}
-                  height=${height}
-                  isScrolling=${isScrolling}
-                  onScroll=${onChildScroll}
-                  scrollTop=${scrollTop}
-                  rowCount=${this.state.sortedMessages.length}
-                  rowHeight=${this._cache.rowHeight}
-                  rowRenderer=${this.rowRenderer}
-                />
-              `;
-            }}
+          <${WindowScroller} scrollElement=${$('.main-view')[0]}>
+            ${({ height, width, isScrolling, onChildScroll, scrollTop }) => html`
+              <${List}
+                autoHeight
+                autoWidth
+                width=${width}
+                height=${height}
+                isScrolling=${isScrolling}
+                onScroll=${onChildScroll}
+                scrollTop=${scrollTop}
+                rowCount=${this.state.sortedMessages.length}
+                rowHeight=${this._cache.rowHeight}
+                rowRenderer=${this.rowRenderer}
+              />
+            `}
           </${WindowScroller}>
       </div>
     `;
