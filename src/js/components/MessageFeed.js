60a77f49c459ad86ede7a739eca4244969473e8b
Umair Mukhtar <34018917+UmairMukhtar@users.noreply.github.com>
2021-07-26 13:31:35 +0500

    Webpack (#67)
    Infinite scrolling improvements by @UmairMukhtar

--- src/js/components/MessageFeed.js
+++ src/js/components/MessageFeed.js
@@ -1,30 +1,62 @@
-import { Component } from "preact";
-import Helpers, { html } from "../Helpers.js";
-import PublicMessage from "./PublicMessage.js";
-import {
-  List,
-  WindowScroller,
-  CellMeasurer,
-  CellMeasurerCache,
-} from "react-virtualized";
-import State from "../State.js";
-import "react-virtualized/styles.css";
-import $ from "jquery";
+import { Component } from 'preact';
+import Helpers, { html } from '../Helpers.js';
+import PublicMessage from './PublicMessage.js';
+import {  List, WindowScroller,CellMeasurer,CellMeasurerCache,} from 'react-virtualized';
+import State from '../State.js';
+import 'react-virtualized/styles.css';
+import $ from 'jquery';
 
 class MessageFeed extends Component {
   constructor() {
     super();
-    this.state = { sortedMessages: [] };
+    this.state = {sortedMessages:[]};
     this.mappedMessages = new Map();
     this.eventListeners = {};
     this._cache = new CellMeasurerCache({
       fixedWidth: true,
-      minHeight: 50,
+      minHeight: 100,
     });
     this.rowRenderer = this.rowRenderer.bind(this);
   }
 
-  rowRenderer = ({ index, key, parent, style }) => {
+  componentDidMount() {
+    this.props.node.map().on((v, k, x, e) => {
+      if (!this.eventListeners['node']) {
+        this.eventListeners['node'] = e;
+      }
+      if (v) {
+        this.mappedMessages.set(k, this.props.keyIsMsgHash ? k : v);
+      } else {
+        this.mappedMessages.delete(k);
+      }
+      this.setState({
+        sortedMessages: Array.from(this.mappedMessages.keys()).sort().reverse().map(k => this.mappedMessages.get(k))
+      })
+    });
+    let first = true;
+    State.local.get('scrollUp').on(() => {
+      !first && Helpers.animateScrollTop('.main-view');
+      first = false;
+    });
+  }
+
+  unsubscribe() {
+    Object.values(this.eventListeners).forEach(e => e.off());
+    this.eventListeners = {};
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.node._.id !== prevProps.node._.id) {
+      this.unsubscribe();
+      this.setState({sortedMessages: [], mappedMessages: new Map()});
+    }
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  rowRenderer = ({ index, key, parent, style,isScrolling }) => {
     const hash = this.state.sortedMessages[index];
     const msg =
       typeof hash === "string"
@@ -45,6 +77,7 @@ class MessageFeed extends Component {
       >
         {({ measure, registerChild }) => (
           <div ref={registerChild} style={style}>
+            {measure()}
             {typeof hash === "string"
               ? html`<${PublicMessage}
                   filter=${this.props.filter}
@@ -61,46 +94,6 @@ class MessageFeed extends Component {
       </CellMeasurer>
     );
   };
-
-  componentDidMount() {
-    this.props.node.map().on((v, k, x, e) => {
-      if (!this.eventListeners["node"]) {
-        this.eventListeners["node"] = e;
-      }
-      if (v) {
-        this.mappedMessages.set(k, this.props.keyIsMsgHash ? k : v);
-      } else {
-        this.mappedMessages.delete(k);
-      }
-      this.setState({
-        sortedMessages: Array.from(this.mappedMessages.keys())
-          .sort()
-          .reverse()
-          .map((k) => this.mappedMessages.get(k)),
-      });
-    });
-    let first = true;
-    State.local.get("scrollUp").on(() => {
-      !first && Helpers.animateScrollTop(".main-view");
-      first = false;
-    });
-  }
-
-  unsubscribe() {
-    Object.values(this.eventListeners).forEach((e) => e.off());
-    this.eventListeners = {};
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.node._.id !== prevProps.node._.id) {
-      this.unsubscribe();
-      this.setState({ sortedMessages: [], mappedMessages: new Map() });
-    }
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
 
   render() {
     return html`
