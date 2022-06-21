1e08e3fc09c6464c0d720d0cb61c7f1456edfee9
Martti Malmi <sirius@iki.fi>
2022-06-21 21:10:16 +0300

    try localStorage for State.local

--- src/js/State.js
+++ src/js/State.js
@@ -19,7 +19,7 @@ const State = {
     if (publicOpts && publicOpts.peers) {
       publicOpts.peers.forEach(url => PeerManager.addPeer({url}));
     }
-    this.local = Gun({peers: [], file: 'State.local', multicast:false, localStorage: false}).get('state');
+    this.local = Gun({peers: [], multicast:false, rad: false, localStorage: true}).get('state');
     if (Helpers.isElectron) {
       this.electron = Gun({peers: ['http://localhost:8768/gun'], file: 'State.electron', multicast:false, localStorage: false}).get('state');
     }
