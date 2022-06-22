d26077992badeadf842da126b991fb92546f0c43
Trevin Hofmann <trevinhofmann@gmail.com>
2022-06-22 06:00:04 -0700

    refactor: convert Name to TypeScript (#132)

---
+++ src/js/components/Name.ts
@@ -0,0 +1,23 @@
+import Component from '../BaseComponent';
+import State from '../State';
+
+type Props = {
+  pub: string;
+  placeholder: string;
+};
+
+type State = {
+  name: string;
+};
+
+class Name extends Component<Props, State> {
+  componentDidMount(): void {
+    State.public.user(this.props.pub).get('profile').get('name').on(this.inject());
+  }
+
+  render() {
+    return this.state.name ?? this.props.placeholder ?? '';
+  }
+}
+
+export default Name;
