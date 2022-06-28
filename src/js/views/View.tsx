80b8e41965103d61fd9273c31974f1169d07f2ca
Martti Malmi <sirius@iki.fi>
2022-06-28 14:34:52 +0300

    view & peersettings tsx

---
+++ src/js/views/View.tsx
@@ -0,0 +1,24 @@
+import Component from '../BaseComponent';
+import { createRef } from 'preact';
+import Header from '../components/Header';
+
+abstract class View extends Component {
+  scrollElement = createRef();
+  class = '';
+  id = '';
+
+  abstract renderView(): JSX.Element;
+
+  render() {
+    return (
+        <>
+          <Header />
+          <div ref={this.scrollElement} class={`main-view ${this.class}`} id={this.id}>
+            {this.renderView()}
+          </div>
+        </>
+    );
+  }
+}
+
+export default View;
