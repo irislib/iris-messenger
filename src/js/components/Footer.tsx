7ebbada78bd8882941056128af8802a6866a88ca
Martti Malmi <sirius@iki.fi>
2022-06-28 12:40:43 +0300

    .

--- src/js/components/Footer.tsx
+++ src/js/components/Footer.tsx
@@ -1,11 +1,10 @@
 import Component from '../BaseComponent';
-import { html } from 'htm/preact';
 import State from '../State';
 import Session from '../Session';
 import Identicon from './Identicon';
 import Icons from '../Icons';
 
-const plusIcon = html`<svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>`;
+const plusIcon = <svg width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>;
 
 type Props = {}
 
@@ -38,7 +37,7 @@ class Footer extends Component<Props, State> {
     const activeRoute = this.state.activeRoute;
 
     if (this.state.chatId) {
-      return html``;
+      return '';
     }
 
     return (
