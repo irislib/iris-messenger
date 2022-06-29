7dfc7035bfb380fc72c6ea3602afca0c5b4cac5f
Martti Malmi <sirius@iki.fi>
2022-06-29 10:16:30 +0300

    fix Helpers.getProfileLink()

--- src/js/Helpers.ts
+++ src/js/Helpers.ts
@@ -2008,7 +2008,7 @@ export default {
   },
 
   getProfileLink(pub: string): string {
-    return `${window.location.origin}/profile/${encodeURIComponent(pub)}`;
+    return `${window.location.origin}/#/profile/${encodeURIComponent(pub)}`;
   },
 
   isElectron,
