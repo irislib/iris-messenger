7ac6ee14512c31565b593fb725d96434faa0ed9a
Martti Malmi <sirius@iki.fi>
2022-09-26 10:25:46 +0300

    move dashboard to its own repo

--- dashboard/components/Npm.tsx
+++
@@ -1,7 +0,0 @@
-export default function Npm() {
-  return (
-    <>
-      <iframe className="mt-3" src="https://npm-stat.com/charts.html?package=iris-lib" width="100%" height="300"></iframe>
-    </>
-  )
-}
