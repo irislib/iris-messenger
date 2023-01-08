63140d7c7da993f7a1132afe766e45db0e1ee7ef
Daniele Tonon <tonon@vitamino.it>
2023-01-09 00:37:02 +0100

    Review mobile bottom toolbar + Search icons
    * Purple color for the "new message" icon
    * Updated feed and global icons
    * Update the search icon

--- src/js/components/Footer.tsx
+++ src/js/components/Footer.tsx
@@ -7,9 +7,14 @@ import Identicon from './Identicon';
 
 const plusIcon = (
   <svg width="24" height="24" viewBox="0 0 24 24">
+    <circle class="nav-plus-bg" cx="12" cy="12" r="12" />
     <path
-      fill="currentColor"
-      d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"
+      fill="#FFFFFF"
+      d="M12,21.1c-0.4,0-0.7-0.3-0.7-0.7V3.7C11.3,3.3,11.6,3,12,3s0.7,0.3,0.7,0.7v16.7C12.7,20.7,12.4,21.1,12,21.1z"
+    />
+    <path
+      fill="#FFFFFF"
+      d="M20.3,12.7H3.7C3.3,12.7,3,12.4,3,12s0.3-0.7,0.7-0.7h16.7c0.4,0,0.7,0.3,0.7,0.7S20.7,12.7,20.3,12.7z"
     />
   </svg>
 );
@@ -75,7 +80,7 @@ class Footer extends Component<Props, State> {
             {plusIcon}
           </a>
           <a href="/discover" class={`btn ${activeRoute === '/discover' ? 'active' : ''}`}>
-            {Icons.search}
+            {Icons.global}
           </a>
           <a
             href={`/profile/${key}`}
