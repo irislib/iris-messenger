24b94568d05cdb7cbf647e286c0222acbd583b2f
Martti Malmi <sirius@iki.fi>
2023-01-10 23:27:45 +0200

    notifications tab active style

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -278,7 +278,7 @@ class Header extends Component {
             ? 'hidden'
             : ''}"
         >
-          ${this.state.activeRoute === '/notifications' ? Icons.heartFull : Icons.heartEmpty}
+          ${Icons.heartEmpty}
           ${this.state.unseenNotificationCount
             ? html`
                 <span class="unseen"
