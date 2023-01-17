233cc962a25abc21f2319a11e5dc7cd17247fdc4
artur <brugeman.artur@gmail.com>
2023-01-17 14:33:48 +0300

    Fix MessageFeed to allow for proper change of the keyword

--- src/js/components/MessageFeed.jsx
+++ src/js/components/MessageFeed.jsx
@@ -76,7 +76,6 @@ class MessageFeed extends Component {
   }
 
   componentDidMount() {
-    console.log("this.props", this.props);
     this.props.scrollElement?.addEventListener('scroll', this.handleScroll);
     let first = true;
     if (this.props.nostrUser) {
@@ -100,7 +99,11 @@ class MessageFeed extends Component {
           }),
         );
       if (this.props.keyword) {
-        Nostr.getMessagesByKeyword(this.props.keyword, messages => this.updateSortedMessages(messages));
+        const keyword = this.props.keyword;
+        Nostr.getMessagesByKeyword(this.props.keyword, messages => {
+	  if (keyword === this.props.keyword)
+	    this.updateSortedMessages(messages);
+	});
       } else if (this.props.index) { // public messages
         if (this.props.index === 'everyone') {
           Nostr.getMessagesByEveryone(messages => this.updateSortedMessages(messages));
