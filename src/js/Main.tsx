76eb25300c300000ecafdbda3430785fd4ae9caa
Martti Malmi <sirius@iki.fi>
2023-01-27 00:15:51 +0200

    commented-out light theme

--- src/js/Main.tsx
+++ src/js/Main.tsx
@@ -3,6 +3,7 @@ import { createHashHistory } from 'history';
 import iris from 'iris-lib';
 import AsyncRoute from 'preact-async-route';
 import { CustomHistory, Router, RouterOnChangeArgs } from 'preact-router';
+
 import '@fontsource/lato';
 
 import Footer from './components/Footer';
