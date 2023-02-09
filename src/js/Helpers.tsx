4c97f47f8168feaf883436a54e233e7fe9823138
Martti Malmi <sirius@iki.fi>
2023-02-09 14:35:20 +0200

    tidal embeds

--- src/js/Helpers.tsx
+++ src/js/Helpers.tsx
@@ -177,6 +177,24 @@ export default {
       );
     });
 
+    const tidalRegex = /(?:https?:\/\/)?(?:www\.)?(?:tidal\.com(?:\/browse)?\/track\/)([\d]+)?/g;
+    replacedText = reactStringReplace(replacedText, tidalRegex, (match, i) => {
+      console.log('tidal', match);
+      return (
+        <iframe
+          class="spotify"
+          key={match + i}
+          width="650"
+          height="200"
+          style={{ maxWidth: '100%' }}
+          src={`https://embed.tidal.com/tracks/${match}?layout=gridify`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      );
+    });
+
     const magnetRegex = /(magnet:\?xt=urn:btih:.*)/gi;
     replacedText = reactStringReplace(replacedText, magnetRegex, (match, i) => {
       // Torrent component
