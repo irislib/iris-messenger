3d16e0d227a5837b2c9a37d4db4370ac08d8fc8d
Martti Malmi <sirius@iki.fi>
2023-02-10 11:48:10 +0200

    set webpack publicpath

--- preact.config.js
+++ preact.config.js
@@ -4,6 +4,8 @@ const path = require('path');
 export default {
   webpack(config, env, helpers, options) {
     config.node = { fs: 'empty' };
+    config.output = config.output || {};
+    config.output.publicPath = '/';
     config.plugins = config.plugins || [];
     config.plugins.push(
       new WebpackBuildNotifierPlugin({
