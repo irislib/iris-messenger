f93b849b30c1fa00c8cdea98e53b9550abf11e42
Martti Malmi <sirius@iki.fi>
2023-02-12 00:45:00 +0200

    embed tweets via twitframe.com

--- src/js/Helpers.tsx
+++ src/js/Helpers.tsx
@@ -111,9 +111,26 @@ export default {
       );
     });
 
-    /*
-    <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-     */
+    const twitterRegex = /(?:^|\s)(?:@)?(https?:\/\/twitter.com\/\w+\/status\/\d+)(?![\w/])/g;
+    replacedText = reactStringReplace(replacedText, twitterRegex, (match, i) => {
+      console.log('tweet match', match);
+      return (
+        <iframe
+          style={{
+            'max-width': '350px',
+            height: '450px',
+            'background-color': 'white',
+            display: 'block',
+          }}
+          key={match + i}
+          border={0}
+          frameborder={0}
+          height={250}
+          width={550}
+          src={`https://twitframe.com/show?url=${encodeURIComponent(match)}`}
+        />
+      );
+    });
 
     const videoRegex = /(https?:\/\/\S+\.(?:mp4|mkv|avi|flv|wmv|mov|webm))/gi;
     replacedText = reactStringReplace(replacedText, videoRegex, (match, i) => {
