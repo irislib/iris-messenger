1c694125c28b11b14c78ab4549b0bbe5c97261a6
Martti Malmi <sirius@iki.fi>
2023-02-16 14:36:16 +0200

    test

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -1,20 +1,12 @@
 import { Context } from "https://edge.netlify.com";
 
-export default async (request, context) => {
-  const headers = new Headers(request.headers);
-  const userAgent = headers.get("User-Agent");
+export default async (request: Request, context: Context) => {
 
-  if (userAgent && userAgent.match(/googlebot|bingbot|yandexbot|slurp|duckduckbot|baiduspider|facebot|ia_archiver|twitterbot|whatsapp/i)) {
-    // If the user agent matches a crawler, modify the request to proxy to the prerender server
-    const url = new URL(request.url);
-    const prerenderUrl = `https://prerender.irismessengers.wtf/${url.href}`;
-    const prerenderRequest = new Request(prerenderUrl, request);
-
-    return fetch(prerenderRequest);
-  } else {
-    // If the user agent does not match a crawler, return the response from the original request
-    const originalResponse = await fetch(request);
-
-    return originalResponse;
-  }
+  const joke = await fetch("https://icanhazdadjoke.com/", {
+    "headers": {
+      "Accept": "application/json"
+    }
+  });
+  const jsonData = await joke.json();
+  return Response.json(jsonData);
 };
