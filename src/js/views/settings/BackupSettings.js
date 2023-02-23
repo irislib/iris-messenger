f54ab20d4bef155ca8ddc8f42e50b66a22f20a79
Martti Malmi <sirius@iki.fi>
2023-02-23 21:11:38 +0200

    import & export backup

--- src/js/views/settings/BackupSettings.js
+++ src/js/views/settings/BackupSettings.js
@@ -1,20 +1,53 @@
 import iris from 'iris-lib';
 
 import Component from '../../BaseComponent';
+import Button from '../../components/basic/Button';
+import CopyButton from '../../components/CopyButton';
 import Identicon from '../../components/Identicon';
 import Name from '../../components/Name';
 import Nostr from '../../Nostr';
 import { translate as t } from '../../translations/Translation';
 
 export default class BackupSettings extends Component {
+  profileExportJson() {
+    const myPub = iris.session.getKey().secp256k1.rpub;
+    let rawDataJson = [];
+    const profileEvent = Nostr.profileEventByUser.get(myPub);
+    const followEvent = Nostr.followEventByUser.get(myPub);
+    profileEvent && rawDataJson.push(profileEvent);
+    followEvent && rawDataJson.push(followEvent);
+    rawDataJson = JSON.stringify(rawDataJson, null, 2);
+    return rawDataJson;
+  }
+
   render() {
     return (
       <>
         <div class="centered-container">
           <h2>{t('backup')}</h2>
-          <h3>{t('import')}</h3>
+          <h3>{t('save')}</h3>
+          <p>{t('save_profile_&_follows')}</p>
           <p>
-            <textarea onInput={(e) => this.onInput(e)} placeholder="[Event JSON]"></textarea>
+            <Button onClick={() => this.onClickDownload()}>{t('download')}</Button>
+          </p>
+          <p>
+            <CopyButton
+              key={`${this.state.hexPub}copyData`}
+              text={t('copy_raw_data')}
+              title={this.state.name}
+              copyStr={() => this.profileExportJson()}
+            />
+          </p>
+
+          <h3>{t('load')}</h3>
+          <p>
+            <Button onClick={() => this.onUploadJsonClick()}>Upload .json file</Button>
+          </p>
+          <p>
+            <textarea
+              onInput={(e) => this.import(e.target.value)}
+              placeholder={t('paste_event_json')}
+            ></textarea>
           </p>
           {this.state.error && <p class="warning">{this.state.error}</p>}
           {this.state.importedEvents && (
@@ -42,9 +75,32 @@ export default class BackupSettings extends Component {
     );
   }
 
-  onInput(event) {
-    // validate json
-    const text = event.target.value;
+  onClickDownload() {
+    const dataStr = 'data:text/json;charset=utf-8,' + encodeURIComponent(this.profileExportJson());
+    const downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute('href', dataStr);
+    downloadAnchorNode.setAttribute('download', 'nostr-backup.json');
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
+
+  onUploadJsonClick() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.json';
+    input.onchange = (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.import(e.target.result);
+      };
+      reader.readAsText(file);
+    };
+    input.click();
+  }
+
+  import(text) {
     if (!text || text.length === 0) {
       this.setState({ error: null, importedEvents: null, restoredFollows: null });
       return;
