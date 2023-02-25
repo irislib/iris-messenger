1872ac444d539d07b2155c5ab516b432235d9fba
Martti Malmi <sirius@iki.fi>
2023-02-25 15:58:03 +0200

    disable twitch autoplay

--- src/js/Helpers.tsx
+++ src/js/Helpers.tsx
@@ -263,7 +263,7 @@ export default {
             width="650"
             height="400"
             style={{ maxWidth: '100%' }}
-            src={`https://player.twitch.tv/?video=${match}&parent=${window.location.hostname}`}
+            src={`https://player.twitch.tv/?video=${match}&parent=${window.location.hostname}&autoplay=false`}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
@@ -284,7 +284,7 @@ export default {
             width="650"
             height="400"
             style={{ maxWidth: '100%' }}
-            src={`https://player.twitch.tv/?channel=${match}&parent=${window.location.hostname}`}
+            src={`https://player.twitch.tv/?channel=${match}&parent=${window.location.hostname}&autoplay=false`}
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
