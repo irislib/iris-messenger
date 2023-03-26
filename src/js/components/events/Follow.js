8bbb15f1a769541dba185a1d184fcf031583499f
Martti Malmi <sirius@iki.fi>
2023-03-26 17:04:19 +0300

    follow event text

--- src/js/components/events/Follow.js
+++ src/js/components/events/Follow.js
@@ -5,6 +5,8 @@ import Key from '../../nostr/Key';
 import Name from '../Name';
 
 export default function Follow(props) {
+  const followsYou = props.event.tags.some((t) => t[0] === 'p' && t[1] === Key.getPubKey());
+  const text = followsYou ? 'started following you' : 'updated their following list';
   return html`
     <div class="msg">
       <div class="msg-content">
@@ -13,7 +15,7 @@ export default function Follow(props) {
           <a href="/${Key.toNostrBech32Address(props.event.pubkey, 'npub')}">
             <${Name} pub=${props.event.pubkey} />
           </a>
-          <span class="mar-left5"> started following you</span>
+          <span class="mar-left5"> ${text}</span>
         </div>
       </div>
     </div>
