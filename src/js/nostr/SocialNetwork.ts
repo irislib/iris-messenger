ac99d5bf42234f9baff0c5094deb00dea48d6651
Martti Malmi <sirius@iki.fi>
2023-04-11 23:47:26 +0300

    fix

--- src/js/nostr/SocialNetwork.ts
+++ src/js/nostr/SocialNetwork.ts
@@ -69,7 +69,10 @@ export default {
       this.usersByFollowDistance.set(distance, new Set());
     }
     if (distance <= 2) {
-      const unsub = PubSub.subscribe({ authors: [user], kinds: [0] }, () => unsub?.(), true);
+      let unsub;
+      // TODO subscribe once param?
+      // eslint-disable-next-line prefer-const
+      unsub = PubSub.subscribe({ authors: [user], kinds: [0] }, () => unsub?.(), true);
     }
     this.usersByFollowDistance.get(distance).add(user);
     // remove from higher distances
