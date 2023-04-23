a974cc514c2a430c49ef9fcd1c39500206ab4741
Martti Malmi <sirius@iki.fi>
2023-04-24 00:03:56 +0300

    feed label styled components

--- src/js/components/feed/Label.tsx
+++ src/js/components/feed/Label.tsx
@@ -1,24 +1,40 @@
+import styled from 'styled-components';
+
 import Icons from '../../Icons';
 import { translate as t } from '../../translations/Translation';
+
+const IconLink = styled.a`
+  padding-right: 10px;
+  color: var(--text-color);
+`;
+
+const SettingsLink = styled.a`
+  padding: 0 10px;
+  color: var(--text-color);
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  text-align: center;
+`;
+
+const FeedName = styled.div`
+  flex: 1;
+`;
 
 export default function Label({ feedName, onClick, index }) {
   const isGeneralFeed = ['global', 'follows'].includes(index);
   return (
     <div className="msg">
       <div className="msg-content notification-msg">
-        <div style="display:flex;flex-direction: row;width:100%;align-items:center;text-align:center;">
-          {isGeneralFeed && (
-            <a href="/" style="padding-right: 10px;color:var(--text-color);">
-              {Icons.backArrow}
-            </a>
-          )}
-          <div style="flex:1;">{t(feedName)}</div>
-          {isGeneralFeed && (
-            <a style="padding: 0 10px;color:var(--text-color);" onClick={onClick}>
-              {Icons.settings}
-            </a>
-          )}
-        </div>
+        <Content>
+          {isGeneralFeed && <IconLink href="/">{Icons.backArrow}</IconLink>}
+          <FeedName>{t(feedName)}</FeedName>
+          {isGeneralFeed && <SettingsLink onClick={onClick}>{Icons.settings}</SettingsLink>}
+        </Content>
       </div>
     </div>
   );
