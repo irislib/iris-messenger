51d4db0811cbc5573526475e261d411a083373de
Martti Malmi <sirius@iki.fi>
2023-04-24 00:19:49 +0300

    feed refactoring

--- src/js/components/feed/Feed.tsx
+++ src/js/components/feed/Feed.tsx
@@ -14,62 +14,83 @@ const TIMESPANS = {
 };
 
 class Feed extends BaseFeed {
-  getEvents(callback) {
-    // different feeds should perhaps be in different components?
-    let since;
+  getSince() {
     if (this.state.settings.timespan !== 'all') {
-      since = Math.floor(Date.now() / 1000) - TIMESPANS[this.state.settings.timespan];
+      return Math.floor(Date.now() / 1000) - TIMESPANS[this.state.settings.timespan];
     }
-    if (this.props.nostrUser) {
-      if (this.props.index === 'likes') {
-        return PubSub.subscribe(
-          // TODO map to liked msg id
-          { authors: [this.props.nostrUser], kinds: [7], since },
-          callback,
-          false,
-          false,
-        );
-      } else {
-        return PubSub.subscribe(
-          { authors: [this.props.nostrUser], kinds: [1, 6], since },
-          (event) => {
-            if (this.props.index === 'posts') {
-              if (Events.getEventReplyingTo(event) && !Events.isRepost(event)) {
-                return;
-              }
-            }
-            callback(event);
-          },
-          false,
-          false,
-        );
+  }
+
+  getCallbackForPostsIndex(callback) {
+    return (event) => {
+      if (Events.getEventReplyingTo(event) && !Events.isRepost(event)) {
+        return;
       }
-    } else if (this.props.keyword) {
-      const keyword = this.props.keyword.toLowerCase();
+      callback(event);
+    };
+  }
+
+  subscribeToNostrUser(since, callback) {
+    if (this.props.index === 'likes') {
       return PubSub.subscribe(
-        { keywords: [keyword], kinds: [1], limit: 1000, since },
-        (e) => e.content?.toLowerCase().includes(keyword) && callback(e), // TODO this should not be necessary. seems subscribe still asks non-search relays
+        { authors: [this.props.nostrUser], kinds: [7], since },
+        callback,
+        false,
         false,
       );
-    } else if (this.props.index === 'follows') {
-      const myPub = Key.getPubKey();
-      const followedUsers = Array.from(SocialNetwork.followedByUser.get(myPub) || []);
-      followedUsers.push(myPub);
-      const filter = { kinds: [1, 6], limit: 300, since, authors: undefined };
-      if (followedUsers.length < 1000) {
-        filter.authors = followedUsers;
-      }
+    } else {
       return PubSub.subscribe(
-        filter,
-        (e) => {
-          if (e.pubkey === myPub || SocialNetwork.followedByUser.get(myPub)?.has(e.pubkey)) {
-            callback(e);
-          }
-        },
-        true,
+        { authors: [this.props.nostrUser], kinds: [1, 6], since },
+        this.getCallbackForPostsIndex(callback),
+        false,
+        false,
       );
     }
+  }
+
+  subscribeToKeyword(since, callback) {
+    const keyword = this.props.keyword.toLowerCase();
+    return PubSub.subscribe(
+      { keywords: [keyword], kinds: [1], limit: 1000, since },
+      (e) => e.content?.toLowerCase().includes(keyword) && callback(e),
+      false,
+    );
+  }
+
+  subscribeToFollows(since, callback) {
+    const myPub = Key.getPubKey();
+    const followedUsers = Array.from(SocialNetwork.followedByUser.get(myPub) || []);
+    followedUsers.push(myPub);
+    const filter = { kinds: [1, 6], limit: 300, since, authors: undefined };
+    if (followedUsers.length < 1000) {
+      filter.authors = followedUsers;
+    }
+    return PubSub.subscribe(
+      filter,
+      (e) => {
+        if (e.pubkey === myPub || SocialNetwork.followedByUser.get(myPub)?.has(e.pubkey)) {
+          callback(e);
+        }
+      },
+      true,
+    );
+  }
+
+  subscribeToGlobalFeed(since, callback) {
     return PubSub.subscribe({ kinds: [1, 6], limit: 300, since }, callback, true);
+  }
+
+  getEvents(callback) {
+    const since = this.getSince();
+
+    if (this.props.nostrUser) {
+      return this.subscribeToNostrUser(since, callback);
+    } else if (this.props.keyword) {
+      return this.subscribeToKeyword(since, callback);
+    } else if (this.props.index === 'follows') {
+      return this.subscribeToFollows(since, callback);
+    } else {
+      return this.subscribeToGlobalFeed(since, callback);
+    }
   }
 }
 
