cd4e0671e1be705ed53be0a63355a68a3f709e61
Keutmann <keutmann@yahoo.com>
2023-07-02 13:45:48 +0200

    Slightly improved the Name component

--- src/js/components/Name.tsx
+++ src/js/components/Name.tsx
@@ -1,10 +1,10 @@
-import { memo, useEffect, useState } from 'react';
+import { memo, useEffect, useState } from "react";
 
-import AnimalName from '../AnimalName';
-import Key from '../nostr/Key';
-import SocialNetwork from '../nostr/SocialNetwork';
+import AnimalName from "../AnimalName";
+import Key from "../nostr/Key";
+import SocialNetwork from "../nostr/SocialNetwork";
 
-import Badge from './Badge';
+import Badge from "./Badge";
 
 type Props = {
   pub: string;
@@ -14,46 +14,61 @@ type Props = {
 
 const Name = (props: Props) => {
   if (!props.pub) {
-    console.error('Name component requires a pub', props);
+    console.error("Name component requires a pub", props);
     return null;
   }
-  const nostrAddr = Key.toNostrHexAddress(props.pub) || '';
-  let initialName = '';
-  let initialDisplayName;
-  let isGenerated = false;
-  const profile = SocialNetwork.profiles.get(nostrAddr);
-  // should we change SocialNetwork.getProfile() and use it here?
-  if (profile) {
-    initialName = profile.name?.trim().slice(0, 100) || '';
-    initialDisplayName = profile.display_name?.trim().slice(0, 100);
-  }
-  if (!initialName) {
-    initialName = AnimalName(Key.toNostrBech32Address(props.pub, 'npub') || props.pub);
-    isGenerated = true;
-  }
-  const [name, setName] = useState(initialName);
-  const [displayName, setDisplayName] = useState(initialDisplayName);
-  const [isNameGenerated, setIsNameGenerated] = useState(isGenerated);
-  useEffect(() => {
-    if (nostrAddr) {
-      // return Unsubscribe function so it unsubs on unmount
-      return SocialNetwork.getProfile(nostrAddr, (profile) => {
-        if (profile) {
-          setName(profile.name?.trim().slice(0, 100) || '');
-          setDisplayName(profile.display_name?.trim().slice(0, 100) || '');
-          setIsNameGenerated(profile.name || profile.display_name ? false : true);
-        }
-      });
+
+
+  const [nostrAddr] = useState(Key.toNostrHexAddress(props.pub) || "");
+  const [profile, setProfile] = useState(profileInitializer);
+  
+  function profileInitializer() {
+    let name;
+    let displayName;
+    let isNameGenerated = false;
+
+    const profile = SocialNetwork.profiles.get(nostrAddr);
+    // should we change SocialNetwork.getProfile() and use it here?
+    if (profile) {
+      name = profile.name?.trim().slice(0, 100) || "";
+      displayName = profile.display_name?.trim().slice(0, 100);
     }
-  }, [props.pub]);
+    if (!name) {
+      name = AnimalName(
+        Key.toNostrBech32Address(props.pub, "npub") || props.pub
+      );
+      isNameGenerated = true;
+    }
+
+    return { name, displayName, isNameGenerated };
+  }
+
+  useEffect(() => {
+    if (!nostrAddr) return;
+    
+    let unsub = SocialNetwork.getProfile(nostrAddr, (p) => {
+      if (p) {
+
+        let name = p.name?.trim().slice(0, 100) || "";
+        let displayName =p.display_name?.trim().slice(0, 100) || "";
+        let isNameGenerated = p.name || p.display_name ? false : true;
+
+        setProfile({ name, displayName, isNameGenerated });
+      }
+    });
+
+    return () => {
+      unsub();
+    }
+  }, [nostrAddr]);
 
   return (
     <>
-      <span className={isNameGenerated ? 'text-neutral-500' : ''}>
-        {name || displayName || props.placeholder}
-      </span>
-      {props.hideBadge ? '' : <Badge pub={props.pub} />}
-    </>
+        <span className={profile.isNameGenerated ? 'text-neutral-500' : ''}>
+          {profile.name || profile.displayName || props.placeholder}
+        </span>
+        {props.hideBadge ? '' : <Badge pub={props.pub} />}
+      </>
   );
 };
 
