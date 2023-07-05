d100845788d9ff02596edcc9354c5bd8c0a35043
Martti Malmi <sirius@iki.fi>
2023-07-05 09:35:10 +0300

    add trending posts to search page

---
+++ src/js/components/feed/SmallFeed.tsx
@@ -0,0 +1,46 @@
+import { FireIcon } from '@heroicons/react/24/solid';
+import { Event, nip19 } from 'nostr-tools';
+import { Link } from 'preact-router';
+
+import Identicon from '../Identicon';
+import Name from '../Name';
+import RelativeTime from '../RelativeTime';
+
+const SmallFeed = ({ events }: { events: Event[] }) => {
+  const mutedUsers = [];
+  return (
+    <div className="card-body p-4">
+      <h2 className="card-title">
+        <FireIcon width={20} className="text-iris-orange" />
+        Trending 24h
+      </h2>
+
+      <hr className="opacity-10" />
+
+      <div className="-ml-2 flex flex-wrap gap-6 text-xs overflow-y-scroll overflow-x-hidden max-h-screen">
+        {events
+          .filter((event) => !mutedUsers[event.pubkey])
+          .map((event) => (
+            <div key={event.id} className="flex gap-2 w-full break-words">
+              <Link href={`/${nip19.npubEncode(event.pubkey)}`}>
+                <Identicon str={event.pubkey} width={30} />
+              </Link>
+              <Link href={`/${nip19.noteEncode(event.id)}`} className="w-full">
+                <b>
+                  <Name pub={event.pubkey} />
+                </b>
+                {' | '}
+                <span className="text-neutral-400">
+                  <RelativeTime date={new Date(event.created_at * 1000)} />
+                  <br />
+                  {event.content?.length > 80 ? `${event.content?.slice(0, 80)}...` : event.content}
+                </span>
+              </Link>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default SmallFeed;
--- src/js/views/Search.tsx
+++ src/js/views/Search.tsx
@@ -1,5 +1,6 @@
 import { route } from 'preact-router';
 
+import SmallFeed from '../components/feed/SmallFeed';
 import SearchBox from '../components/SearchBox';
 
 import View from './View';
@@ -7,24 +8,31 @@ import View from './View';
 class Search extends View {
   constructor() {
     super();
-    this.state = { sortedMessages: [] };
+    this.state = { trendingPosts: [] };
     this.id = 'message-view';
     this.class = 'public-messages-view';
   }
 
   componentDidMount() {
     this.restoreScrollPosition();
+    fetch('https://api.nostr.band/v0/trending/notes')
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data?.notes) return;
+        this.setState({ trendingPosts: data.notes.map((e) => e.event) });
+      });
   }
 
   renderView() {
     return (
-      <div class="mt-2 mx-2 md:mx-0">
+      <div class="mt-2 mx-2 md:mx-0 flex flex-col gap-4">
         <SearchBox
           focus={true}
           onSelect={({ key }) => {
             route('/' + key);
           }}
         />
+        <SmallFeed events={this.state.trendingPosts} />
       </div>
     );
   }
