9b7aeffc2cf19be7b74b0b9352e8d8a84a52b908
Martti Malmi <sirius@iki.fi>
2023-07-05 10:23:57 +0300

    profile pic style

--- src/js/components/ProfilePicture.tsx
+++ src/js/components/ProfilePicture.tsx
@@ -25,14 +25,14 @@ const ProfilePicture = ({ picture, onError }: Props) => {
       <SafeImg
         width={128}
         square={true}
-        className="rounded-full"
+        className="rounded-full cursor-pointer"
         src={picture}
         onError={onError}
         onClick={handleClick}
       />
       {showModal && (
         <Modal centerVertically={true} onClose={handleClose}>
-          <SafeImg square={true} src={picture} onError={onError} />
+          <SafeImg className="max-w-full max-h-[90vh]" square={true} src={picture} onError={onError} />
         </Modal>
       )}
     </>
