6f6f157ad3ed24e5c14b577571ca4a3ed1e0ba4b
Martti Malmi <sirius@iki.fi>
2023-07-21 16:52:22 +0300

    add safe area to show new messages floating btn

--- src/js/components/feed/ShowNewEvents.tsx
+++ src/js/components/feed/ShowNewEvents.tsx
@@ -2,7 +2,7 @@ import { translate as t } from '../../translations/Translation.mjs';
 
 function ShowNewEvents({ onClick }) {
   return (
-    <div className="fixed bottom-16 md:bottom-8 justify-center items-center z-10 flex w-full md:w-1/2">
+    <div className="fixed bottom-16 md:bottom-8 justify-center items-center z-10 flex w-full md:w-1/2 pb-safe-area">
       <div
         className="btn btn-sm opacity-90 hover:opacity-100 hover:bg-iris-blue bg-iris-blue text-white"
         onClick={onClick}
