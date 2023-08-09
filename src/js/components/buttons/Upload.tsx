6da28a95f23ac4a4b5c7aca8a2a7fb3bcd5f4c6d
Martti Malmi <sirius@iki.fi>
2023-08-09 17:50:17 +0300

    allow upload img by pasting into public message field

--- src/js/components/buttons/Upload.tsx
+++ src/js/components/buttons/Upload.tsx
@@ -1,27 +1,24 @@
 import { useState } from 'react';
 
+import { uploadFile } from '@/utils/uploadFile';
+
 const Upload = (props) => {
   const [error, setError] = useState('');
+
   const handleFileUpload = (event) => {
     const files = event.target.files || event.dataTransfer.files;
     if (files && files.length) {
-      const formData = new FormData();
-      formData.append('fileToUpload', files[0]);
-
-      fetch('https://nostr.build/api/upload/iris.php', {
-        method: 'POST',
-        body: formData,
-      })
-        .then(async (response) => {
-          const url = await response.json();
-          if (url && props.onUrl) {
+      uploadFile(
+        files[0],
+        (url) => {
+          if (props.onUrl) {
             props.onUrl(url);
           }
-        })
-        .catch((error) => {
-          console.error('upload error', error);
-          setError('upload failed: ' + JSON.stringify(error));
-        });
+        },
+        (errorMsg) => {
+          setError(errorMsg);
+        },
+      );
     }
   };
 
