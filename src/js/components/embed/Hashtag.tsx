ab69673026cd9421c46ef45c5d8ff1fda701a74c
Martti Malmi <sirius@iki.fi>
2023-08-19 08:24:31 +0300

    .

--- src/js/components/embed/Hashtag.tsx
+++ src/js/components/embed/Hashtag.tsx
@@ -3,10 +3,8 @@ import { Link } from 'preact-router';
 import Embed from './index';
 
 const Hashtag: Embed = {
-  // Using a non-capturing group for prefix and capturing everything after
   regex: /(?:\s|^)(#\w+)/g,
   component: ({ match }) => {
-    // Split the prefix and the hashtag using the first character (assuming it's always #)
     return (
       <>
         {' '}
