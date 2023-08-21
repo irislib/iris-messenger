949c14a3c60b08b13e51a5d1984ec905d99fc3ab
Keutmann <keutmann@yahoo.com>
2023-08-21 12:30:03 +0200

    Implementing useKey

--- src/js/components/user/Badge.tsx
+++ src/js/components/user/Badge.tsx
@@ -1,24 +1,25 @@
 import { JSX } from 'preact';
 
-import Key from '../../nostr/Key';
+import { useKey } from '@/hooks/useKey';
+
 import SocialNetwork from '../../nostr/SocialNetwork';
 import { translate as t } from '../../translations/Translation.mjs';
 import Icons from '../../utils/Icons';
 
 export default function Badge(props): JSX.Element | null {
-  const myPub = Key.getPubKey();
-  const hexAddress = Key.toNostrHexAddress(props.pub);
-  if (hexAddress === myPub) {
+  const { hexKey, isMe, myPubKey } = useKey(props.pub);
+  
+  if(!props.pub) return null; // If no pub key, don't render anything
+
+  if (isMe) {
     return (
       <span class="mx-2 text-iris-blue tooltip" data-tip={t('you')}>
         {Icons.checkmark}
       </span>
     );
   }
-  if (!hexAddress) {
-    return null;
-  }
-  const following = SocialNetwork.isFollowing(myPub, hexAddress);
+
+  const following = SocialNetwork.isFollowing(myPubKey, hexKey);
   if (following) {
     return (
       <span class="ml-2 text-iris-blue tooltip" data-tip={t('following')}>
@@ -26,7 +27,7 @@ export default function Badge(props): JSX.Element | null {
       </span>
     );
   } else {
-    const count = SocialNetwork.followedByFriendsCount(hexAddress);
+    const count = SocialNetwork.followedByFriendsCount(hexKey);
     if (count > 0) {
       const className = count > 10 ? 'text-iris-orange' : '';
       return (
--- src/js/utils/UniqueIds.ts
+++ src/js/utils/UniqueIds.ts
@@ -1,5 +1,8 @@
+import Key from "@/nostr/Key";
+
 // should this be a class instead? convert all strings to internal representation, enable comparison
 export type UID = number;
+
 
 // save space by mapping strs to internal unique ids
 export class UniqueIds {
@@ -8,9 +11,13 @@ export class UniqueIds {
   static currentUniqueId = 0;
 
   static id(str: string): UID {
-    if (str.startsWith('npub')) {
-      throw new Error('use hex instead of npub ' + str);
+    if(!str) throw new Error('ID(str) is undefined');
+
+    if (str.startsWith('npub') || str.startsWith('note') || str.startsWith('nsec')) {
+      str = Key.toNostrHexAddress(str) as string; // Convert to hex
+      if(!str) throw new Error('ID(str) is invalid or empty');
     }
+
     const existing = UniqueIds.strToUniqueId.get(str);
     if (existing) {
       return existing;
@@ -32,7 +39,13 @@ export class UniqueIds {
   static has(str: string): boolean {
     return UniqueIds.strToUniqueId.has(str);
   }
+
+  static bech32(id: number, prefix: string = 'npub'): string {
+    return Key.toNostrBech32Address(UniqueIds.str(id), prefix) || '';
+  }
+
 }
 
 export const STR = UniqueIds.str;
 export const ID = UniqueIds.id;
+export const BECH32 = UniqueIds.bech32;
